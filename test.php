84af02991cc933ab30107ebc7933b80e8866f63c
Marcio Santos Correia <marcio-1109@hotmail.com>
2014-03-07 19:05:22 -0300

    Gerar pdf mas não mostra dados

--- test.php
+++ test.php
@@ -1,29 +1,31 @@
-<?php
-
-
-   require_once("classes/db.class.php");
+﻿<?php 
+ 
+   /*require_once("classes/db.class.php");
    //date_default_timezone_set('America/Sao_Paulo'); 
    
 
      $banco= new DB;
 	 $banco->conectaDB();
 	 $conecta=$banco;
-	 if ($conecta !==FALSE){
-		$sql="SELECT * FROM patrimonio WHERE situacao = 1 ";
-		$res=mysql_query($sql,$conecta);
-		  
-		if ($res===FALSE){
-			echo"<h3 align='center'>Não há patrimônios ativos!</h3><a href='relatorio.php'><input type='button' value='Voltar' /></a>
-<hr />";
-		}else{
-			$tabela='';
-			while($patrimonio=mysql_fetch_assoc($res)){
-					$tabela.="<tr> <td>".$patrimonio['num_patrimonio']."</td><td>".$patrimonio['tipo']."</td><td>".$patrimonio['num_posicionamento']."</td><td>".$patrimonio['situacao']."</td></tr>";
-					
+	 if($conecta!==FALSE){
+		 $sql="SELECT * FROM Patrimonio WHERE situacao = 1";
+		 $res=mysql_query($sql,$conecta);
+		 	if ($res===FALSE){
+			echo"<h3 align='center'>Não há patrimonios ativos</h3><a href='relatorio.php'><input type='button' value='Voltar' /></a> <hr />";
 			}
-		}
-	 
-	  $relatorio='
+		    else{
+				   $tabela='';
+					while($patrimonio=mysql_fetch_assoc($res)){
+						
+						$tabela.="<tr> <td>".$patrimonio["num_patrimonio"]."</td><td>".$patrimonio["tipo"]."</td><td>".$patrimonio["num_posicionamento"]."</td><td>".$patrimonio["situacao"]."</td></tr>";
+						
+						}				
+				}
+	} */
+	
+	
+	
+  $relatorio='
 <html>
 <head>
 <style>
@@ -179,16 +181,23 @@
     </style>
 </head>
 <body>
-<hr>';
+<hr>
 
+        <h2 align="center"> Relatório de Bens Ativos</h2>';
+
+
+
+
+$relatorio.='
+<table align="center">
  
-    $relatorio.='
+ </table>
  
   <br>
-  
+  <br> 
  <table align="center">
  <tr>
- <th>Número do patrimônio </th> <th>Tipo</th> <th>Número de posicionamento</th> <th>Situação</th>
+ <th>Número do patrimônio </th> <th>Tipo</th> <th>Nº de posicionamento</th> <th>Situação</th>
  </tr>
  '.$tabela.'
   </table>
@@ -198,55 +207,23 @@
 <hr />
 <div id="footer"> 
     <table>
-        <tr><td>Restart</td></tr>
+        <tr><td>Paróquia Santa Lúcia</td></tr>
     </table>
 </div>
 </htmlpagefooter>
 <sethtmlpagefooter name="footer" value="on" />
 </body>
 </html>
-
 ';
-	  
- 
+  
+  
   include('MPDF57/mpdf.php');
   $mpdf=new mPDF('c','A4','','' , 0 , 0 , 0 , 0 , 0 , 0); 
- 
-$mpdf->SetDisplayMode('fullpage');
- 
-$mpdf->list_indent_first_level = 0; 
+  $mpdf->SetDisplayMode('fullpage');
+  $mpdf->list_indent_first_level = 0; 
   $mpdf->WriteHTML($relatorio);
   $mpdf->Output();
   exit();
-	
-   }else{
-	  $mostrarhtml="sim";
-   }
-   if ($mostrarhtml=='sim'){
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Restart</title>
-<hr />
-<b><h2 align="center">Gerar Relatório de Patrimônios Ativos</h2></b>
-<hr />
-</head>
-
-
-<body>
-<form action="relatorio.php" method="post">
-
-
- <input type="submit" value="Gerar" />
- <hr />
- <a href="painel.php">
- <input type="button" value="Voltar" /></a>
- </form>
+  
+ ?>
  
-</body>
-</html>
-<?php
-   }
-?>
